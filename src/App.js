1c449461d1924b27e95a1a629857c93ad038db2a
zvezdd <qwiszvezd@gmail.com>
2024-08-18 23:56:30 +0600

    header

--- src/App.js
+++ src/App.js
@@ -3,7 +3,18 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+      <header className="header">
+        <section className="ul-section">
+          <ul>
+            <li>Read Comics</li>
+            <li>Take a quiz</li>
+            <li>View rankings</li>
+            <li>Chat</li>
+          </ul>
+        </section>
+      </header>
       <h1>Protectoons</h1>
+      <footer></footer>
     </div>
   );
 }
