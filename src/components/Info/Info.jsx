404c0a6b27bc1201686ed54851d8c16f27b53265
zvezdd <qwiszvezd@gmail.com>
2024-08-24 21:53:47 +0600

    features block

--- src/components/Info/Info.jsx
+++ src/components/Info/Info.jsx
@@ -1,13 +1,32 @@
-import React from 'react'
-import './Info.css'
+import React from "react";
+import "./Info.css";
 
 export default function Info() {
   return (
     <div>
-        <div className="about-us">
-            <span className='us'>About us</span>
-            <span className='desc'>ProtecToons is a team of educators, artists, and child rights advocates dedicated to using storytelling to inspire and inform. Our interactive comics offer a fun and effective way for children to learn about their rights. We stand out by providing high-quality, accessible content that makes learning about rights enjoyable and easy for all ages. Our project aims to educate children about rights through engaging and humorous comics.</span>
+      <div className="about-us">
+        <span className="us">About us</span>
+        <span className="desc">
+          ProtecToons is a team of educators, artists, and child rights
+          advocates dedicated to using storytelling to inspire and inform. Our
+          interactive comics offer a fun and effective way for children to learn
+          about their rights. We stand out by providing high-quality, accessible
+          content that makes learning about rights enjoyable and easy for all
+          ages. Our project aims to educate children about rights through
+          engaging and humorous comics.
+        </span>
+      </div>
+      <div className="features">
+        <span className="h1">Features</span>
+        <div className="rewards">
+          <span>Fun comics</span>
+          <span>Anonymous sharing of your story</span>
+          <span>Rights information</span>
+          <span>Informative quizzes</span>
+          <span>Rewards</span>
+          <span>Interactive platform</span>
         </div>
+      </div>
     </div>
-  )
+  );
 }
