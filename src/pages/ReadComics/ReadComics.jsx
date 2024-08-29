aa994bd158db9a8a928f82d91f9ff4be2a845230
zvezdd <qwiszvezd@gmail.com>
2024-08-29 21:57:43 +0600

    comics

--- src/pages/ReadComics/ReadComics.jsx
+++ src/pages/ReadComics/ReadComics.jsx
@@ -1,8 +1,21 @@
-import React from 'react'
-import './ReadComics.css'
+import React from "react";
+import "./ReadComics.css";
 
 export default function ReadComics() {
   return (
-    <div className="readcomics">ReadComics</div>
-  )
+    <div className="readcomics">
+      <div className="our-heroes">
+        <span className="comicstitle">Our Heroes: Victims of Injustice</span>
+        <br />
+        <span className="comicssubtitle">
+          Our heroes are ordinary kids who have faced injustice and chaos in
+          their lives. They are no longer willing to close their eyes to the
+          problems around them and are determined to fight for their rights.
+          Read these gripping stories about how our heroes overcome challenges,
+          find support, and deal with injustice, making the world a better and
+          fairer place.
+        </span>
+      </div>
+    </div>
+  );
 }
